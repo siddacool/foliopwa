8c6619b9bc2884b2ecbe3363e6b7d32f1865067f
Siddhesh Mangela <sidthecool007@gmail.com>
2018-05-15 12:25:42 +0530

    mozila tut followed

--- sw.js
+++ sw.js
@@ -1,24 +1,50 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }),
-  );
-}).catch((e) => {
+var cacheName = 'folio-admin-v1';
+var appShellFiles = [
+  '/',
+  '/admin-panel.html',
+  '/site.js',
+  '/public/dist/build/login.js',
+  '/public/dist/build/login.css',
+  '/public/dist/favicon/favicon.ico',
+  '/public/dist/favicon/favicon.png',
+  '/public/dist/favicon/icon-192.png',
+];
 
+var contentToCache = appShellFiles;
+
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(contentToCache);
+    })
+  );
 });
 
-self.addEventListener('install', (e) => {
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.open('the-magic-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/admin-panel.html',
-        '/public/dist/favicon/manifest.json',
-        '/site.js',
-        '/public/dist/build/login.js',
-        '/public/dist/build/login.css',
-      ]);
-    }),
+    caches.keys().then(function(keyList) {
+          return Promise.all(keyList.map(function(key) {
+        if(cacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
