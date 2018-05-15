8e2ddd90d2e590c0b91a8582f7086441768a8080
Siddhesh Mangela <sidthecool007@gmail.com>
2018-05-15 11:41:08 +0530

    caching added

--- sw.js
+++ sw.js
@@ -1,4 +1,22 @@
-/** An empty service worker! */
 self.addEventListener('fetch', function(event) {
-  /** An empty fetch handler! */
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    });
+  );
+});
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('the-magic-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/admin-panel.html',
+        '/public/dist/favicon/manifest.json',
+        '/site.js',
+        '/public/dist/build/login.js',
+        '/public/dist/build/login.css',
+      ]);
+    }),
+  );
 });
