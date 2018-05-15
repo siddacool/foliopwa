9aca7e599d1658c56ec1329a13831e25bced467c
Siddhesh Mangela <sidthecool007@gmail.com>
2018-05-15 15:10:04 +0530

    compressed assets added

--- sw.js
+++ sw.js
@@ -1,16 +1,36 @@
-var cacheName = 'folio-admin-v1';
+var cacheName = 'folio-admin-login562352698ba83a7d6920js-w';
 var appShellFiles = [
   '/foliopwa/',
   '/foliopwa/admin-panel.html',
   '/foliopwa/site.js',
-  '/foliopwa/public/dist/build/login.js',
-  '/foliopwa/public/dist/build/login.css',
+  '/foliopwa/public/dist/build/login.562352698ba83a7d6920.js',
+  '/foliopwa/public/dist/build/login.bc0758175b238f5f99c0994a46e11867.css',
   '/foliopwa/public/dist/favicon/favicon.ico',
   '/foliopwa/public/dist/favicon/favicon.png',
+  '/foliopwa/public/dist/favicon/icon-32.png',
+  '/foliopwa/public/dist/favicon/icon-64.png',
+  '/foliopwa/public/dist/favicon/icon-96.png',
+  '/foliopwa/public/dist/favicon/icon-128.png',
   '/foliopwa/public/dist/favicon/icon-192.png',
+  '/foliopwa/public/dist/favicon/icon-256.png',
+  '/foliopwa/public/dist/favicon/icon-512.png',
+  'https://fonts.googleapis.com/css?family=Montserrat:400,600,700,800',
+  'https://www.gstatic.com/firebasejs/4.12.0/firebase.js',
 ];
 
 var contentToCache = appShellFiles;
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+        if (cacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
 
 self.addEventListener('install', function(e) {
   console.log('[Service Worker] Install');
